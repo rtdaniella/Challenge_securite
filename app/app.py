c28bd23ed08c2161c06e9707173bd1014f0624b0
Daniella Rakotondratsimba <daniellark11@gmail.com>
2025-03-10 22:20:16 +0100

    update app - onglet donnees

--- app/app.py
+++ app/app.py
@@ -5,30 +5,33 @@ st.set_page_config(layout="wide")
 
 with st.sidebar:
 
-    #st.image("assets/images/logo.png", use_container_width=True)
+    st.image("assets/logo.png", use_container_width=True)
 
     selected = option_menu(
         "", 
-        ["Accueil", "Analyse", "Machine Learning", "Daniella", "Linh-Nhi", "Edina"],
-        icons=["house", "graph-up", "bar-chart-steps", "bar-chart-steps", "bar-chart-steps", "bar-chart-steps"],
+        ["📊 Dashboard", "📄 Données", "📈 Analyse réseau", "🛡️ Détection d'anomalies", "Linh-Nhi", "Edina", "Accueil"],
+        icons=["📊", "📄", "📈", "🛡️","🧑‍💻","🧑‍💻"],
         menu_icon="none",
     )
 
-if selected == "Accueil":
-    import accueil
-    accueil.show_accueil()
+if selected == "📊 Dashboard":
+    import dashboard
+    dashboard.show_dashboard()
+elif selected == "📄 Données":
+    import explore_data
+    explore_data.show_data()
 elif selected == "Analyse":
     import analyse
     analyse.show_analyse()
 elif selected == "Model":
     import model
     model.show_model()
-elif selected == "Daniella":
-    import daniella
-    daniella.show_daniella()
-elif selected == "Linh-Nhi":
+elif selected == "📈 Analyse réseau":
     import linhnhi
     linhnhi.show_linhnhi()
 elif selected == "Edina":
     import edina
     edina.show_edina()
+elif selected == "Accueil":
+    import accueil
+    accueil.show_accueil()
